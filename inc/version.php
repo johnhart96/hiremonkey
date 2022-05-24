2ea675539e6b799d16cb2fcc25fdbc7370e72faf
John Hart <john@jh96.co.uk>
2022-05-24 16:57:23 +0100

    Build 1.0.0.23

--- inc/version.php
+++ inc/version.php
@@ -15,7 +15,7 @@ if( file_exists( "package.json" ) ) {
 }
 define( "BUILD_STATUS" , "beta" ); 
 define( "LICENCEVERSION" , 1 ); 
-define( "FULLBUILD" , "1.0.0.22" );
+define( "FULLBUILD" , "1.0.0.23" );
 
 // Important table list
 $tables = array(
