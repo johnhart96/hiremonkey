3b2a657c8ac3bd292bcb844c05a0e1ec8224c25f
John Hart <john@jh96.co.uk>
2022-07-15 14:14:59 +0100

    Increasing build number

--- inc/version.php
+++ inc/version.php
@@ -15,7 +15,7 @@ if( file_exists( "package.json" ) ) {
 }
 define( "BUILD_STATUS" , "beta" ); 
 define( "LICENCEVERSION" , 1 ); 
-define( "FULLBUILD" , "1.0.0.23" );
+define( "FULLBUILD" , "1.0.0.24" );
 
 // Important table list
 $tables = array(
